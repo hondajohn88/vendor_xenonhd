3d97d9a1a2e73813ee34a039427341b1733a9101
hondajohn88 <hondajohn88@gmail.com>
2017-10-31 10:23:35 -0700

    Add chiron to vendor setup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,7 @@ add_lunch_combo xenonhd_athene-userdebug
 add_lunch_combo xenonhd_bacon-userdebug
 add_lunch_combo xenonhd_bullhead-userdebug
 add_lunch_combo xenonhd_cancro-userdebug
+add_lunch_combo xenonhd_chiron-userdebug
 add_lunch_combo xenonhd_d855-userdebug
 add_lunch_combo xenonhd_endeavoru-userdebug
 add_lunch_combo xenonhd_fortuna3g-userdebug
